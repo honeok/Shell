a52b4db61fad08369abcf3f12fb10ccacb844de7
honeok <yihaohey@gmail.com>
2024-11-05 14:11:28 +0800

    up

--- jds/game_allstop.sh
+++ jds/game_allstop.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+## Author: honeok
+## Blog: www.honeok.com
 
-yellow='\033[1;33m'
-red='\033[1;31m'
-green='\033[1;32m'
-white='\033[0m'
+## Debug
+## set -x
+
+yellow='\033[93m'        # 亮黄色
+red='\033[91m'           # 亮红色
+green='\033[92m'         # 亮绿色
+white='\033[0m'          # 重置
 _yellow() { echo -e ${yellow}$@${white}; }
 _red() { echo -e ${red}$@${white}; }
 _green() { echo -e ${green}$@${white}; }
 
 cd /data/tool/
 if pgrep -f processcontrol-allserver.sh >/dev/null 2>&1; then
-	pkill -9 -f processcontrol-allserver.sh
-	> control.txt
-	> dump.txt
-	_green "processcontrol进程已终止文件已清空"
+    pkill -9 -f processcontrol-allserver.sh
+    > control.txt
+    > dump.txt
+    _green "processcontrol进程已终止文件已清空"
 else
-	_red "processcontrol进程未运行无需终止"
+    _red "processcontrol进程未运行无需终止"
 fi
 
 cd /data/server/login/ && ./server.sh stop
@@ -24,13 +29,13 @@ cd /data/server/gate/ && ./server.sh stop && sleep 120s
 _green "login和gate服务器已停止"
 
 for i in {1..5}; do
-	(
-		_yellow "正在处理server$i"
-		cd /data/server$i/game/
-		./server.sh flush
-		sleep 60s
-		./server.sh stop
-	) &
+    (
+        _yellow "正在处理server$i"
+        cd /data/server$i/game/
+        ./server.sh flush
+        sleep 60s
+        ./server.sh stop
+    ) &
 done
 
 # 等待所有并行操作完成
